f539254c915390ba874ba5561840128e2bd79df0
Punit Sakre <punitsakre786@gmail.com>
2024-04-25 18:24:41 +0530

    initial code flow for job service

---
+++ job-service/src/main/java/com/microservice/jobservice/job/JobController.java
@@ -0,0 +1,63 @@
+package com.microservice.jobservice.job;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/jobs")
+public class JobController {
+
+    private final JobService jobService;
+
+    public JobController(JobService jobService) {
+        this.jobService = jobService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Job>> findAll() {
+        return ResponseEntity.ok(jobService.findAll());
+    }
+
+    @PostMapping
+    public ResponseEntity<String> createJob(@RequestBody Job job) {
+        jobService.createJob(job);
+        return new ResponseEntity<>("Job Created Successfully!", HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Job> getJobById(@PathVariable Long id) {
+        Job job = jobService.getJobById(id);
+        return new ResponseEntity<>(job, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteJobById(@PathVariable Long id) {
+        if (jobService.deleteJobById(id)) {
+            return ResponseEntity.ok("Job has been deleted successfully!");
+        }
+        else {
+            return new ResponseEntity<>("Job Not Found", HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PutMapping("/{id}")
+//    @RequestMapping(method = RequestMethod.PUT, value = "/jobs/{id}")
+    public ResponseEntity<String> updateJobById(@PathVariable Long id, @RequestBody Job job) {
+        if (jobService.updateJobById(id, job)) {
+            return ResponseEntity.ok("Job updated successfully!");
+        }
+        else {
+            return new ResponseEntity<>("Job not found!", HttpStatus.NOT_FOUND);
+        }
+    }
+}
---
+++ job-service/src/main/java/com/microservice/jobservice/job/JobService.java
@@ -0,0 +1,16 @@
+package com.microservice.jobservice.job;
+
+import java.util.List;
+
+public interface JobService {
+
+    List<Job> findAll();
+
+    void createJob(Job job);
+
+    Job getJobById(Long id);
+
+    boolean deleteJobById(Long id);
+
+    boolean updateJobById(Long id, Job job);
+}

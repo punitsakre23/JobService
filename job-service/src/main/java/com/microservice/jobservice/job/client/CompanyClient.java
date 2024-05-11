8a5488e2f10b2912effcbec7074ddf472ccb62cb
Punit Sakre <punitsakre786@gmail.com>
2024-05-12 01:44:18 +0530

    modifying mapper method

---
+++ job-service/src/main/java/com/microservice/jobservice/job/client/CompanyClient.java
@@ -0,0 +1,13 @@
+package com.microservice.jobservice.job.client;
+
+import com.microservice.jobservice.job.external.Company;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "COMPANY-SERVICE")
+public interface CompanyClient {
+
+    @GetMapping("/companies/{id}")
+    Company getCompanyById(@PathVariable("id") Long id);
+}
